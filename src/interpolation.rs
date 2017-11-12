59a41cb9277069ef6228afccf392631053e2ac2c
Ryan Leach <ryan.n.leach@gmail.com>
2017-11-11 19:11:18 -0700

    Added dendritic snow zone.

---
+++ src/interpolation.rs
@@ -0,0 +1,118 @@
+
+use sounding_base::{Sounding, DataRow, OptionVal};
+
+/// Interpolate values from the vertical sounding using pressure as the primary coordinate.
+///
+/// Returns a `DataRow` struct with interpolated values.
+pub fn linear_interpolate(snd: &Sounding, target_p: f64) -> DataRow {
+
+    use sounding_base::Profile::*;
+
+    let pressure = snd.get_profile(Pressure);
+    let temperature = snd.get_profile(Temperature);
+    let wet_bulb = snd.get_profile(WetBulb);
+    let dew_point = snd.get_profile(DewPoint);
+    let theta_e = snd.get_profile(ThetaE);
+    let direction = snd.get_profile(WindDirection);
+    let speed = snd.get_profile(WindSpeed);
+    let omega = snd.get_profile(PressureVerticalVelocity);
+    let height = snd.get_profile(GeopotentialHeight);
+    let cloud_fraction = snd.get_profile(CloudFraction);
+
+    let mut result = DataRow::default();
+    result.pressure = OptionVal::from(target_p);
+
+    let mut below_idx: usize = 0;
+    let mut above_idx: usize = 0;
+    for (i, p) in pressure.iter().enumerate() {
+        if let Some(p) = p.as_option() {
+            if p > target_p {
+                below_idx = i;
+            }
+            if p < target_p {
+                above_idx = i;
+                break;
+            }
+        }
+    }
+
+    if above_idx != 0 {
+        let p_below = pressure[below_idx].unwrap();
+        let p_above = pressure[above_idx].unwrap();
+        let run = p_above - p_below;
+        let dp = target_p - p_below;
+
+        result.temperature = eval_linear_interp(below_idx, above_idx, run, dp, temperature);
+        result.wet_bulb = eval_linear_interp(below_idx, above_idx, run, dp, wet_bulb);
+        result.dew_point = eval_linear_interp(below_idx, above_idx, run, dp, dew_point);
+        result.theta_e = eval_linear_interp(below_idx, above_idx, run, dp, theta_e);
+
+        // Special interpolation for anlges
+        if direction.len() > above_idx {
+            if let (Some(dir_below), Some(dir_above)) =
+                (
+                    direction[below_idx].as_option(),
+                    direction[above_idx].as_option(),
+                )
+            {
+                let x_below = dir_below.to_radians().sin();
+                let x_above = dir_above.to_radians().sin();
+                let y_below = dir_below.to_radians().cos();
+                let y_above = dir_above.to_radians().cos();
+
+                let rise_x = x_above - x_below;
+                let rise_y = y_above - y_below;
+
+                let x_dir = x_below + dp * rise_x / run;
+                let y_dir = y_below + dp * rise_y / run;
+
+                let mut dir = x_dir.atan2(y_dir).to_degrees();
+
+                while dir < 0.0 {
+                    dir += 360.0;
+                }
+                while dir > 360.0 {
+                    dir -= 360.0;
+                }
+
+                result.direction = dir.into();
+            }
+        }
+
+        result.speed = eval_linear_interp(below_idx, above_idx, run, dp, speed);
+        result.omega = eval_linear_interp(below_idx, above_idx, run, dp, omega);
+        result.height = eval_linear_interp(below_idx, above_idx, run, dp, height);
+        result.cloud_fraction = eval_linear_interp(below_idx, above_idx, run, dp, cloud_fraction);
+    }
+
+    result
+}
+
+fn eval_linear_interp(
+    blw_idx: usize,
+    abv_idx: usize,
+    run: f64,
+    dp: f64,
+    array: &[OptionVal<f64>],
+) -> OptionVal<f64> {
+    if array.len() > abv_idx {
+        if let (Some(val_below), Some(val_above)) =
+            (array[blw_idx].as_option(), array[abv_idx].as_option())
+        {
+            let rise = val_above - val_below;
+            OptionVal::from(val_below + dp * rise / run)
+        } else {
+            OptionVal::default()
+        }
+    } else {
+        OptionVal::default()
+    }
+}
+
+pub fn linear_interp(x_val: f64, x1: f64, x2: f64, y1: f64, y2: f64) -> f64 {
+    let run = x2 - x1;
+    let rise = y2 - y1;
+    let dx = x_val - x1;
+
+    y1 + rise / run * dx
+}
---
+++ src/snow.rs
@@ -0,0 +1,62 @@
+use sounding_base::{Sounding, Profile};
+use sounding_base::Profile::*;
+
+/// Find the dendtritic growth zones throughout the profile. It is unusual, but possible there is
+/// more than 1!
+pub fn dendritic_growth_zone(snd: &Sounding, v_coord: Profile) -> Vec<(f64, f64)> {
+    let mut result = Vec::with_capacity(2);
+
+    // Dendritic snow growth zone temperature range in C
+    const WARM_SIDE: f64 = -12.0;
+    const COLD_SIDE: f64 = -18.0;
+
+    let mut profile = snd.get_profile(Temperature).iter().zip(
+        snd.get_profile(v_coord)
+            .iter(),
+    );
+
+    let mut bottom = ::std::f64::MAX; // Only initialize because compiler can't tell value not used
+    let mut top: f64;
+
+    // Initialize the bottom of the sounding
+    let mut last_t: f64;
+    let mut last_coord: f64;
+    loop {
+        if let Some((t, coord)) = profile.by_ref().next() {
+            if let (Some(t), Some(coord)) = (t.as_option(), coord.as_option()) {
+                last_t = t;
+                last_coord = coord;
+                break;
+            }
+        }
+    }
+
+    // Check to see if we are already in the dendtritic zone
+    if last_t < WARM_SIDE && last_t > COLD_SIDE {
+        bottom = last_coord;
+    }
+
+    for (t, coord) in profile {
+        if let (Some(t), Some(coord)) = (t.as_option(), coord.as_option()) {
+            if last_t > WARM_SIDE && t < WARM_SIDE {
+                // Just crossed into a dendritic zone
+                bottom = ::interpolation::linear_interp(WARM_SIDE, last_t, t, last_coord, coord);
+            }
+            if last_t > COLD_SIDE && t < COLD_SIDE {
+                // Just crossed out of a dendritic zone
+                top = ::interpolation::linear_interp(COLD_SIDE, last_t, t, last_coord, coord);
+                result.push((bottom, top));
+            }
+            last_t = t;
+            last_coord = coord;
+        }
+    }
+
+    // Check to see if we ended in a dendtritic zone
+    if last_t < WARM_SIDE && last_t > COLD_SIDE {
+        top = last_coord;
+        result.push((bottom, top));
+    }
+
+    result
+}
