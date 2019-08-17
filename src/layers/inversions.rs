e61b24a96b09f0e6074482b844b9ea9e5cceda25
Ryan <rnleach@users.noreply.github.com>
2019-08-17 10:58:30 -0600

    Clean up inversion layers.

--- src/layers/inversions.rs
+++ src/layers/inversions.rs
@@ -3,13 +3,11 @@ use crate::{
     error::{AnalysisError, Result},
     sounding::Sounding,
 };
-use itertools::izip;
-use metfor::{Celsius, HectoPascal};
+use itertools::{izip, Itertools};
+use metfor::HectoPascal;
 
 /// Get all inversion layers up to a specified pressure.
 pub fn inversions(snd: &Sounding, top_p: HectoPascal) -> Result<Layers> {
-    let mut to_return: Layers = Layers::new();
-
     let t_profile = snd.temperature_profile();
     let p_profile = snd.pressure_profile();
 
@@ -18,39 +16,59 @@ pub fn inversions(snd: &Sounding, top_p: HectoPascal) -> Result<Layers> {
     }
 
     izip!(0usize.., p_profile, t_profile)
-        // Filter out rows without both temperature and pressure.
-        .filter_map(|triple| {
-            if triple.1.is_some() && triple.2.is_some() {
-                let (i, p, t) = (triple.0, triple.1.unpack(), triple.2.unpack());
-                Some((i, p, t))
+        // Filter out levels with missing data
+        .filter(|&(_, p, t)| p.is_some() && t.is_some())
+        // Unpack from the `Optioned` type
+        .map(|(i, p, t)| (i, p.unpack(), t.unpack()))
+        // Stop once we reach the top level pressure
+        .take_while(|&(_, p, _)| p >= top_p)
+        // Now all we need is the row index and the temperature
+        .map(|(i, _, t)| (i, t))
+        // Inspect the levels in pairs
+        .tuple_windows::<(_, _)>()
+        // Map into inversion types
+        .map(|((i0, t0), (i1, t1))| {
+            if t1 > t0 {
+                LayerType::Inversion(i0, i1)
             } else {
-                None
+                LayerType::NonInversion
             }
         })
-        // Filter out rows above the top pressure
-        .filter_map(|(i, p, t)| if p < top_p { None } else { Some((i, t)) })
-        // Capture the inversion layers
-        .fold(
-            (0, Celsius(std::f64::MAX), None),
-            |(last_i, last_t, mut bottom_opt), (i, t)| {
-                if bottom_opt.is_none() && last_t < t {
-                    // Coming into an inversion
-                    bottom_opt = snd.data_row(last_i);
-                } else if bottom_opt.is_some() && last_t > t {
-                    // Leaving an inversion
-                    if let Some(layer) = bottom_opt.and_then(|bottom| {
-                        snd.data_row(last_i)
-                            .and_then(|top| Some(Layer { bottom, top }))
-                    }) {
-                        to_return.push(layer);
-                        bottom_opt = None;
+        // Combine adjacent inversion layers into a single, larger inversion
+        .scan(
+            (None, None),
+            |layers: &mut (Option<usize>, Option<usize>), layer: LayerType| {
+                let bottom: &mut Option<usize> = &mut layers.0;
+                let top: &mut Option<usize> = &mut layers.1;
+
+                match layer {
+                    LayerType::Inversion(i0, i1) => {
+                        if bottom.is_none() {
+                            *bottom = Some(i0);
+                        }
+                        *top = Some(i1);
+                        Some(None)
+                    }
+                    LayerType::NonInversion => {
+                        if let (Some(i0), Some(i1)) = (bottom.take(), top.take()) {
+                            Some(Some((i0, i1)))
+                        } else {
+                            Some(None)
+                        }
                     }
                 }
-                (i, t, bottom_opt)
             },
-        );
-
-    Ok(to_return)
+        )
+        // Remove iterations that did not capture an inverison
+        .filter_map(|opt| opt)
+        // Map indexes into layers
+        .map(|(i0, i1)| {
+            snd.data_row(i0)
+                .and_then(|bottom| snd.data_row(i1).map(|top| Layer { bottom, top }))
+        })
+        // Transform from `Option` to `Result`
+        .map(|opt| opt.ok_or(AnalysisError::InvalidInput))
+        .collect()
 }
 
 /// Get a surface based inversion.
@@ -63,68 +81,48 @@ pub fn sfc_based_inversion(snd: &Sounding) -> Result<Option<Layer>> {
         return Err(AnalysisError::MissingProfile);
     }
 
-    izip!(0usize.., p_profile, h_profile, t_profile)
+    let sfc_layer = izip!(0usize.., p_profile, h_profile, t_profile)
         // Remove levels with missing data
-        .filter_map(|tuple| {
-            if tuple.1.is_some() && tuple.2.is_some() && tuple.2.is_some() {
-                Some(tuple.0)
+        .filter(|(_, p, h, t)| p.is_some() && h.is_some() && t.is_some())
+        // Unpack from `Optioned` type and discard the height, we no longer need it.
+        .map(|(i, p, _, t)| (i, p.unpack(), t.unpack()))
+        // Only look up to about 700 hPa
+        .take_while(|(_, p, _)| *p > HectoPascal(690.0))
+        // Pair them up to look at them as a layer
+        .tuple_windows::<(_, _)>()
+        // Map into inversion types
+        .map(|((i0, _, t0), (i1, _, t1))| {
+            if t1 > t0 {
+                LayerType::Inversion(i0, i1)
             } else {
-                None
+                LayerType::NonInversion
             }
         })
-        // Get the first one.
-        .nth(0)
-        // Map Option to Result
-        .ok_or(AnalysisError::NotEnoughData)
-        // Map the result into a data row
-        .and_then(|index| snd.data_row(index).ok_or(AnalysisError::MissingValue))
-        // Now find the top
-        .and_then(|bottom_row| {
-            let sfc_t = bottom_row.temperature;
-            if sfc_t.is_some() {
-                let sfc_t = sfc_t.unpack();
-                let val = izip!(0usize.., p_profile, t_profile, h_profile)
-                    // Remove levels with missing data
-                    .filter_map(|tuple| {
-                        if tuple.1.is_some() && tuple.2.is_some() && tuple.3.is_some() {
-                            let (i, p, t) = (tuple.0, tuple.1.unpack(), tuple.2.unpack());
-                            Some((i, p, t))
-                        } else {
-                            None
-                        }
-                    })
-                    // This is the first one!
-                    .skip(1)
-                    // Only look up to about 700 hPa
-                    .take_while(|(_, p, _)| *p > HectoPascal(690.0))
-                    // Remove those cooler than the surface
-                    .filter(|(_, _, t)| *t > sfc_t)
-                    .fold(None, |max_t_info, (i, _, t)| {
-                        if let Some((max_t, _max_t_idx)) = max_t_info {
-                            if t > max_t {
-                                Some((t, i))
-                            } else {
-                                max_t_info
-                            }
-                        } else {
-                            Some((t, i))
-                        }
-                    });
+        // Only take inversion layers to be combined later, if the first one isn't an inversion,
+        // then this isn't a surface based inversion!
+        .take_while(|lyr| {
+            std::mem::discriminant(lyr) == std::mem::discriminant(&LayerType::Inversion(0, 0))
+        })
+        // Unwrap from the inversion type
+        .map(|lyr| match lyr {
+            LayerType::Inversion(i0, i1) => (i0, i1),
+            LayerType::NonInversion => unreachable!(),
+        })
+        // Now combine the layers into one
+        .fold(None, |combined_layers, (i0, i1)| match combined_layers {
+            None => Some((i0, i1)),
+            Some((bottom, _top)) => Some((bottom, i1)),
+        }) // Option<(usize, usize)
+        .and_then(|(bottom_i, top_i)| {
+            let bottom = snd.data_row(bottom_i)?;
+            let top = snd.data_row(top_i)?;
+            Some(Layer { bottom, top })
+        });
 
-                match val {
-                    Some((_, idx)) => snd
-                        .data_row(idx)
-                        .ok_or(AnalysisError::MissingValue)
-                        .and_then(|top_row| {
-                            Ok(Some(Layer {
-                                bottom: bottom_row,
-                                top: top_row,
-                            }))
-                        }),
-                    None => Ok(None),
-                }
-            } else {
-                Err(AnalysisError::MissingValue)
-            }
-        })
+    Ok(sfc_layer)
+}
+
+enum LayerType {
+    Inversion(usize, usize),
+    NonInversion,
 }
