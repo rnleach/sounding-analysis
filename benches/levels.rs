6027ccf5e9b25eb9d12a3f154b2e5736dcd811b2
Ryan <rnleach@users.noreply.github.com>
2021-03-28 15:36:43 -0600

    Switch to Criterion.rs for benchmarking.

--- benches/levels.rs
+++ benches/levels.rs
@@ -1,32 +1,40 @@
-#![feature(test)]
+//! Run these benches with `cargo bench --bench funtions -- --verbose`
+//!
+//! Run with `cargo bench --bench funtions -- --verbose vapor_pressure_over_ice` to select the
+//! single benchmark.
 
-extern crate test;
-use test::Bencher;
+use criterion::{criterion_group, criterion_main, Criterion};
 
 mod utils;
 
-#[bench]
-fn test_freezing_levels(b: &mut Bencher) {
+criterion_main!(levels_benches);
+
+criterion_group!(
+    levels_benches,
+    freezing_levels_bench,
+    max_temperature_in_profile_bench
+);
+
+fn freezing_levels_bench(c: &mut Criterion) {
     let snds = utils::load_all_test_files();
 
-    let test = || {
-        for snd in &snds {
-            sounding_analysis::freezing_levels(&snd).expect("oops");
-        }
-    };
-
-    b.iter(test)
+    c.bench_function("freezing_levels", |b| {
+        b.iter(|| {
+            for snd in &snds {
+                sounding_analysis::freezing_levels(&snd).expect("oops");
+            }
+        });
+    });
 }
 
-#[bench]
-fn test_max_temperature_in_profile(b: &mut Bencher) {
+fn max_temperature_in_profile_bench(c: &mut Criterion) {
     let snds = utils::load_all_test_files();
 
-    let test = || {
-        for snd in &snds {
-            sounding_analysis::max_temperature_in_profile(&snd).expect("oops");
-        }
-    };
-
-    b.iter(test)
+    c.bench_function("max_temperature_in_profile", |b| {
+        b.iter(|| {
+            for snd in &snds {
+                sounding_analysis::max_temperature_in_profile(&snd).expect("oops");
+            }
+        });
+    });
 }
