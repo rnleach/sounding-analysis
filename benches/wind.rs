d61979a2b3ed300314c6654a578c54052e8ad943
Ryan <rnleach@users.noreply.github.com>
2021-04-09 16:03:15 -0600

    Add wind benches.

---
+++ benches/wind.rs
@@ -0,0 +1,84 @@
+//! Run these benches with `cargo bench --bench wind -- --verbose`
+use criterion::{criterion_group, criterion_main, Criterion};
+
+mod utils;
+
+criterion_main!(wind_benches);
+
+criterion_group!(
+    wind_benches,
+    mean_wind_bench,
+    sr_helicity_bench,
+    bunkers_storm_motion_bench
+);
+
+fn mean_wind_bench(c: &mut Criterion) {
+    let snds = utils::load_all_test_files().to_vec();
+    let pairs = snds
+        .into_iter()
+        .filter_map(|snd| {
+            sounding_analysis::pressure_layer(
+                &snd,
+                metfor::HectoPascal(850.0),
+                metfor::HectoPascal(700.0),
+            )
+            .ok()
+            .map(|lyr| (snd, lyr))
+        })
+        .filter(|(_, lyr)| lyr.bottom.height.is_some())
+        .collect::<Vec<_>>();
+
+    assert!(!pairs.is_empty());
+
+    c.bench_function("mean_wind", |b| {
+        b.iter(|| {
+            for (snd, layer) in &pairs {
+                let _x = sounding_analysis::mean_wind(layer, &snd).expect("oops");
+            }
+        });
+    });
+}
+
+fn sr_helicity_bench(c: &mut Criterion) {
+    let snds = utils::load_all_test_files().to_vec();
+    let pairs = snds
+        .into_iter()
+        .filter_map(|snd| {
+            sounding_analysis::pressure_layer(
+                &snd,
+                metfor::HectoPascal(850.0),
+                metfor::HectoPascal(700.0),
+            )
+            .ok()
+            .map(|lyr| (snd, lyr))
+        })
+        .filter(|(_, lyr)| lyr.bottom.height.is_some())
+        .collect::<Vec<_>>();
+
+    let storm_motion: metfor::WindUV<metfor::MetersPSec> = metfor::WindUV {
+        u: metfor::MetersPSec(6.0),
+        v: metfor::MetersPSec(6.0),
+    };
+
+    assert!(!pairs.is_empty());
+
+    c.bench_function("sr_helicity", |b| {
+        b.iter(|| {
+            for (snd, layer) in &pairs {
+                let _x = sounding_analysis::sr_helicity(layer, storm_motion, &snd).expect("oops");
+            }
+        });
+    });
+}
+
+fn bunkers_storm_motion_bench(c: &mut Criterion) {
+    let snds = utils::load_all_test_files();
+
+    c.bench_function("bunkers_storm_motion", |b| {
+        b.iter(|| {
+            for snd in &snds {
+                let _x = sounding_analysis::bunkers_storm_motion(&snd);
+            }
+        });
+    });
+}
