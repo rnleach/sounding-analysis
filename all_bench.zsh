91a84b50d57978d5672ae601dad8bcd4cfd2f60f
Ryan <rnleach@users.noreply.github.com>
2021-04-16 09:10:28 -0600

    Fix benches script.

--- all_bench.zsh
+++ all_bench.zsh
@@ -1,45 +1,45 @@
-#!/usr/bin/env zsh -l
+#!/usr/bin/env zsh
 
 setopt verbose
 
-BASELINE_TAG = "opt-start"
-COMPARE_TAG = "master"
+BASELINE_TAG="opt-start"
+COMPARE_TAG="master"
 
-BENCHES = (indexes layers levels parcel parcel_profile profile wind fire)
+BENCHES=("indexes" "layers" "levels" "parcel" "parcel_profile" "profile" "wind" "fire")
 
 
 # Check out the baseline tag.
 git checkout ${BASELINE_TAG}
 
 # Force generic compile target.
-export RUSTFLAGS="-C target-cpu=generic"
+RUSTFLAGS_VAL="-C target-cpu=generic"
 
 # Do a warm up run.
-cargo bench
+RUSTFLAGS=${RUSTFLAGS_VAL} cargo bench
 
 # Do the real benches
 BASELINE="${BASELINE_TAG}-generic"
 for bench_name in ${BENCHES}; do
-    cargo bench --bench ${bench_name} -- --save-baseline ${BASELINE}
+    RUSTFLAGS=${RUSTFLAGS_VAL} cargo bench --bench ${bench_name} -- --save-baseline ${BASELINE_TAG}
 done
 
 # Force to compile to target CPU
-export RUSTFLAGS="-C target-cpu=native"
+RUSTFLAGS_VAL="-C target-cpu=native"
 
 # Do a warm up run.
-cargo bench
+RUSTFLAGS=${RUSTFLAGS_VAL} cargo bench
 
 # Do the real benches
 BASELINE="${BASELINE_TAG}-native"
 for bench_name in ${BENCHES}; do
-    cargo bench --bench ${bench_name} -- --save-baseline ${BASELINE}
+    RUSTFLAGS=${RUSTFLAGS_VAL} cargo bench --bench ${bench_name} -- --save-baseline ${BASELINE_TAG}
 done
 
 # Check out the comparison tag
 git checkout ${COMPARE_TAG}
 
 # Force generic compile target.
-export RUSTFLAGS="-C target-cpu=generic"
+RUSTFLAGS_VAL="-C target-cpu=generic"
 
 # Do a warm up run.
 cargo bench
@@ -47,11 +47,11 @@ cargo bench
 # Do the real benches
 BASELINE="${COMPARE_TAG}-generic"
 for bench_name in ${BENCHES}; do
-    cargo bench --bench ${bench_name} -- --save-baseline ${BASELINE}
+    RUSTFLAGS=${RUSTFLAGS_VAL} cargo bench --bench ${bench_name} -- --save-baseline ${BASELINE_TAG}
 done
 
 # Force to compile to target CPU
-export RUSTFLAGS="-C target-cpu=native"
+RUSTFLAGS_VAL="-C target-cpu=native"
 
 # Do a warm up run.
 cargo bench
@@ -59,7 +59,7 @@ cargo bench
 # Do the real benches
 BASELINE="${COMPARE_TAG}-native"
 for bench_name in ${BENCHES}; do
-    cargo bench --bench ${bench_name} -- --save-baseline ${BASELINE}
+    RUSTFLAGS=${RUSTFLAGS_VAL} cargo bench --bench ${bench_name} -- --save-baseline ${BASELINE_TAG}
 done
 
 
